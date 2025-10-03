3149b7b876be81941339bfbe60395e00913c3f97
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-03 17:02:54 -0500

    Initial commit

---
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/WasteTrackPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecolutions.platform.wastetrackplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteTrackPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteTrackPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecolutions/platform/wastetrackplatform/WasteTrackPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecolutions.platform.wastetrackplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteTrackPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
