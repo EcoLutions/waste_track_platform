45db245ec46a1d54acd8b4795aeada3c96ce1f09
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-07 15:15:40 -0500

    feat(community-relations): introduce evidence commands and queries
    Add `UploadEvidenceCommand` for handling evidence uploads with validation.
    Add queries (`GetEvidenceByIdQuery` and `GetAllEvidencesQuery`) for retrieving evidence data.
    Update `EvidenceCommandService` to handle `UploadEvidenceCommand`.
    Introduce `EvidenceQueryService` for processing evidence-related queries.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/communityrelations/domain/services/command/EvidenceCommandService.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/communityrelations/domain/services/command/EvidenceCommandService.java
@@ -1,17 +1,10 @@
 package com.ecolutions.platform.wastetrackplatform.communityrelations.domain.services.command;
 
-import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.commands.CreateEvidenceCommand;
-import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.commands.DeleteEvidenceCommand;
-import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.commands.UpdateEvidenceCommand;
+import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.commands.UploadEvidenceCommand;
 import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.entities.Evidence;
 
 import java.util.Optional;
 
 public interface EvidenceCommandService {
-
-    Optional<Evidence> handle(CreateEvidenceCommand command);
-
-    Optional<Evidence> handle(UpdateEvidenceCommand command);
-
-    Boolean handle(DeleteEvidenceCommand command);
+    Optional<Evidence> handle(UploadEvidenceCommand command);
 }
