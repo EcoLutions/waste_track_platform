c782cf4a9b7b7ce52f837e1287a7ec116ac9b563
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-07 11:27:25 -0500

    feat(community-relations): add report query and command interfaces
    Introduce `ReportQueryService` to handle queries for getting all reports and a single report by ID.
    Introduce `ReportCommandService` to handle commands for creating, updating, and deleting reports.
    These interfaces define the contracts for managing reports in the community relations domain.

---
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/communityrelations/domain/services/queries/ReportQueryService.java
@@ -0,0 +1,13 @@
+package com.ecolutions.platform.wastetrackplatform.communityrelations.domain.services.queries;
+
+import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.aggregates.Report;
+import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.queries.GetAllReportsQuery;
+import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.queries.GetReportByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReportQueryService {
+    Optional<Report> handle(GetReportByIdQuery query);
+    List<Report> handle(GetAllReportsQuery query);
+}
