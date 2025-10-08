77e729f3b1463f67dba4aa8b1fc12d5c7a18227e
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-08 15:55:56 -0500

    feat(iam): implement query services for role and user retrieval
    - Add `RoleQueryServiceImpl` for fetching all roles and roles by name.
    - Extend `UserQueryServiceImpl` functionality to include user retrieval by username.
    - Refactor `UserQueryServiceImpl` to simplify code by removing unnecessary exception handling.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/iam/application/internal/queryservices/UserQueryServiceImpl.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/iam/application/internal/queryservices/UserQueryServiceImpl.java
@@ -1,36 +1,39 @@
 package com.ecolutions.platform.wastetrackplatform.iam.application.internal.queryservices;
 
+
 import com.ecolutions.platform.wastetrackplatform.iam.domain.model.aggregates.User;
 import com.ecolutions.platform.wastetrackplatform.iam.domain.model.queries.GetAllUsersQuery;
 import com.ecolutions.platform.wastetrackplatform.iam.domain.model.queries.GetUserByIdQuery;
-import com.ecolutions.platform.wastetrackplatform.iam.domain.services.queries.UserQueryService;
+import com.ecolutions.platform.wastetrackplatform.iam.domain.model.queries.GetUserByUsernameQuery;
+import com.ecolutions.platform.wastetrackplatform.iam.domain.model.valueobjects.Username;
+import com.ecolutions.platform.wastetrackplatform.iam.domain.services.UserQueryService;
 import com.ecolutions.platform.wastetrackplatform.iam.infrastructure.persistence.jpa.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class UserQueryServiceImpl implements UserQueryService {
     private final UserRepository userRepository;
 
-    @Override
-    public Optional<User> handle(GetUserByIdQuery query) {
-        try {
-            return userRepository.findById(query.userId());
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Failed to retrieve user: " + e.getMessage(), e);
-        }
+    public UserQueryServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public List<User> handle(GetAllUsersQuery query) {
-        try {
-            return userRepository.findAll();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Failed to retrieve users: " + e.getMessage(), e);
-        }
+        return userRepository.findAll();
+    }
+
+
+    @Override
+    public Optional<User> handle(GetUserByIdQuery query) {
+        return userRepository.findById(query.userId());
+    }
+
+    @Override
+    public Optional<User> handle(GetUserByUsernameQuery query) {
+        return userRepository.findByUsername(new Username(query.username()));
     }
 }
