415cd6029b14cbe8c1b6348715319e331917c9f0
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-08 15:43:45 -0500

    feat(api): enhance OpenAPI config and introduce JWT authentication setup
    - Dynamically configure OpenAPI metadata (title, description, version) using `application.properties`.
    - Add support for bearer token authentication via OpenAPI security schemes.
    - Update Swagger documentation with dynamic app-specific details.
    - Integrate JWT dependencies for token-based authentication.
    - Update application properties and POM to support JWT configurations.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,27 +1,50 @@
 package com.ecolutions.platform.wastetrackplatform.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${documentation.application.description}")
+    String applicationDescription;
+
+    @Value("${documentation.application.version}")
+    String applicationVersion;
+
     @Bean
     public OpenAPI platformOpenApi() {
         var openApi = new OpenAPI();
-        openApi
-                .info(new Info()
-                        .title("WasteTrack Platform API")
-                        .description("API documentation for the WasteTrack Platform service")
-                        .version("v1.0.0")
-                        .license(new License().name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+        openApi.info(new Info()
+                        .title(applicationName)
+                        .description(applicationDescription)
+                        .version(applicationVersion)
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("WasteTrack Platform Documentation")
-                        .url("https://ecolutions.com/docs/wastetrack-platform"));
+                        .description(applicationName + " Documentation")
+                        .url("https://"+applicationName.toLowerCase()+".com/docs"));
+
+        // Add security definitions
+        String securitySchemeName = "bearerAuth";
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+        // Return the OpenAPI object with the configuration
         return openApi;
     }
 }
