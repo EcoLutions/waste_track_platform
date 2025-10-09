fc7d699ee41d692605f531fa02a100d73985798f
Kyrubi <u20221c486@upc.edu.pe>
2025-10-09 02:18:55 -0500

    test(runner): simplify Cucumber test runner configuration

--- src/test/java/com/ecolutions/platform/wastetrackplatform/acceptance/tests/CucumberTestRunner.java
+++ src/test/java/com/ecolutions/platform/wastetrackplatform/acceptance/tests/CucumberTestRunner.java
@@ -1,10 +1,8 @@
 package com.ecolutions.platform.wastetrackplatform.acceptance.tests;
 
-import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @Suite
 @IncludeEngines("cucumber")  // Indica que use el engine de Cucumber
