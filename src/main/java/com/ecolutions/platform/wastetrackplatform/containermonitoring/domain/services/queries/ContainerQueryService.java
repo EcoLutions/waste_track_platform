565cc2de62a846b5148cb71849fcced29bcf78d6
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-10 11:29:40 -0500

    feat(container-monitoring): add query support for fetching containers in alert by district ID
    Implements `GetContainersInAlertByDistrictIdQuery` and updates `ContainerQueryService` to handle container retrieval in alert status. Adds `findContainersInAlert` method in `ContainerRepository` to support querying based on alert conditions.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/containermonitoring/domain/services/queries/ContainerQueryService.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/containermonitoring/domain/services/queries/ContainerQueryService.java
@@ -4,6 +4,7 @@ import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.mod
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.queries.GetAllContainersByDistrictIdQuery;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.queries.GetContainerByIdQuery;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.queries.GetAllContainersQuery;
+import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.queries.GetContainersInAlertByDistrictIdQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +13,5 @@ public interface ContainerQueryService {
     Optional<Container> handle(GetContainerByIdQuery query);
     List<Container> handle(GetAllContainersQuery query);
     List<Container> handle(GetAllContainersByDistrictIdQuery query);
+    List<Container> handle(GetContainersInAlertByDistrictIdQuery query);
 }
