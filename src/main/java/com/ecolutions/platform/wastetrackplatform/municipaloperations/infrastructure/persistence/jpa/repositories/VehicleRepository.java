89f3c0ed69e80a3f3709ae1a20e229a46f160f30
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-10 12:18:31 -0500

    feat(municipal-operations): add query support for fetching vehicles by district ID
    Implements the `findByDistrictId` method in `VehicleRepository` to enable querying vehicles by district ID. Updates `VehicleQueryService` and its implementation to handle `GetAllVehiclesByDistrictIdQuery`.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/municipaloperations/infrastructure/persistence/jpa/repositories/VehicleRepository.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/municipaloperations/infrastructure/persistence/jpa/repositories/VehicleRepository.java
@@ -1,9 +1,13 @@
 package com.ecolutions.platform.wastetrackplatform.municipaloperations.infrastructure.persistence.jpa.repositories;
 
 import com.ecolutions.platform.wastetrackplatform.municipaloperations.domain.model.aggregates.Vehicle;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.DistrictId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, String> {
+    List<Vehicle> findByDistrictId(DistrictId districtId);
 }
