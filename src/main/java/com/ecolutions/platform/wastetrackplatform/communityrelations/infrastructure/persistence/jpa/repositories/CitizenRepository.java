3a0565443c01166f076b0e772571c475d564319d
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-10 16:16:49 -0500

    feat(community-relations): add query support for fetching citizens by district ID
    Implements `findByDistrictId` method in `CitizenRepository` to enable querying citizens by district ID. Updates `CitizenQueryService` and its implementation to handle `GetAllCitizensByDistrictIdQuery`.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/communityrelations/infrastructure/persistence/jpa/repositories/CitizenRepository.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/communityrelations/infrastructure/persistence/jpa/repositories/CitizenRepository.java
@@ -1,9 +1,13 @@
 package com.ecolutions.platform.wastetrackplatform.communityrelations.infrastructure.persistence.jpa.repositories;
 
 import com.ecolutions.platform.wastetrackplatform.communityrelations.domain.model.aggregates.Citizen;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.DistrictId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CitizenRepository extends JpaRepository<Citizen, String> {
+    List<Citizen> findByDistrictId(DistrictId districtId);
 }
