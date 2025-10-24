5036b18a86a9626b44f45e4f7589f929d3ffe7a2
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-24 14:41:14 -0500

    refactor(user): add findByEmail method to UserProfileRepository for email-based user retrieval

--- src/main/java/com/ecolutions/platform/wastetrackplatform/profile/infrastructure/persistence/jpa/repositories/UserProfileRepository.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/profile/infrastructure/persistence/jpa/repositories/UserProfileRepository.java
@@ -1,6 +1,7 @@
 package com.ecolutions.platform.wastetrackplatform.profile.infrastructure.persistence.jpa.repositories;
 
 import com.ecolutions.platform.wastetrackplatform.profile.domain.model.aggregates.UserProfile;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.EmailAddress;
 import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, String> {
     Optional<UserProfile> findByUserId(UserId userId);
+    Optional<UserProfile> findByEmail(EmailAddress emailAddress);
 }
