ab29c4ef0fc4624e43a607e6511ed8d2ae278b3b
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-07 18:09:51 -0500

    refactor(paymentsubscriptions): update BillingPeriod import to use shared domain model
    Replace BillingPeriod import from paymentsubscriptions domain model with the shared domain model to ensure better reusability and consistency across modules.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/paymentsubcriptions/interfaces/rest/mappers/fromentitytoresponse/PlanCatalogResourceFromEntityAssembler.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/paymentsubcriptions/interfaces/rest/mappers/fromentitytoresponse/PlanCatalogResourceFromEntityAssembler.java
@@ -1,7 +1,7 @@
 package com.ecolutions.platform.wastetrackplatform.paymentsubcriptions.interfaces.rest.mappers.fromentitytoresponse;
 
 import com.ecolutions.platform.wastetrackplatform.paymentsubcriptions.domain.model.entities.PlanCatalog;
-import com.ecolutions.platform.wastetrackplatform.paymentsubcriptions.domain.model.valueobjects.BillingPeriod;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.BillingPeriod;
 import com.ecolutions.platform.wastetrackplatform.paymentsubcriptions.interfaces.rest.dto.response.PlanCatalogResource;
 import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.Money;
 
