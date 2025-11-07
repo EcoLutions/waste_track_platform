df4901d5f3177a431acec9bd08ada063764cb227
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-07 17:57:28 -0500

    refactor(district): update CreateDistrictCommandFromResourceAssembler and UpdateDistrictResource to use primaryAdminUsername and remove unnecessary fields

--- src/main/java/com/ecolutions/platform/wastetrackplatform/municipaloperations/interfaces/rest/mappers/fromresourcetocommand/CreateDistrictCommandFromResourceAssembler.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/municipaloperations/interfaces/rest/mappers/fromresourcetocommand/CreateDistrictCommandFromResourceAssembler.java
@@ -1,17 +1,17 @@
 package com.ecolutions.platform.wastetrackplatform.municipaloperations.interfaces.rest.mappers.fromresourcetocommand;
 
 import com.ecolutions.platform.wastetrackplatform.municipaloperations.domain.model.commands.CreateDistrictCommand;
-import com.ecolutions.platform.wastetrackplatform.municipaloperations.domain.model.valueobjects.GeographicBoundaries;
 import com.ecolutions.platform.wastetrackplatform.municipaloperations.interfaces.rest.dto.request.CreateDistrictResource;
 import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.EmailAddress;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.Username;
 
 public class CreateDistrictCommandFromResourceAssembler {
     public static CreateDistrictCommand toCommandFromResource(CreateDistrictResource resource) {
         return new CreateDistrictCommand(
             resource.name(),
             resource.code(),
-            new GeographicBoundaries(resource.boundaries()),
             EmailAddress.of(resource.primaryAdminEmail()),
+            Username.of(resource.primaryAdminUsername()),
             resource.planId()
         );
     }
