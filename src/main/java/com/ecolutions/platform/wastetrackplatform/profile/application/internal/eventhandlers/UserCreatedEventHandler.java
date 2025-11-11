1348e24743b102ef8f118e0d9d3165a516b1e147
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-11 13:06:13 -0500

    feat(event-handler): modify UserCreatedEventHandler to initialize user profile only if hasProfile is true

--- src/main/java/com/ecolutions/platform/wastetrackplatform/profile/application/internal/eventhandlers/UserCreatedEventHandler.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/profile/application/internal/eventhandlers/UserCreatedEventHandler.java
@@ -3,7 +3,6 @@ package com.ecolutions.platform.wastetrackplatform.profile.application.internal.
 import com.ecolutions.platform.wastetrackplatform.iam.domain.model.events.UserCreatedEvent;
 import com.ecolutions.platform.wastetrackplatform.profile.domain.model.commands.InitializeUserProfileCommand;
 import com.ecolutions.platform.wastetrackplatform.profile.domain.services.command.UserProfileCommandService;
-import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.Roles;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -19,16 +18,9 @@ public class UserCreatedEventHandler {
     @EventListener(UserCreatedEvent.class)
     @Async
     public void on(UserCreatedEvent event) {
-        var roles = event.getRoles().stream().map(Roles::fromString).toList();
+        if(!event.getHasProfile()) return;
 
-        var isSystemAdministrator = roles.contains(Roles.ROLE_SYSTEM_ADMINISTRATOR);
-        if (isSystemAdministrator) {return;}
-
-        var command = new InitializeUserProfileCommand(
-                event.getUserId(),
-                event.getEmail(),
-                event.getDistrictId()
-        );
+        var command = new InitializeUserProfileCommand(event.getUserId(), event.getEmail(), event.getDistrictId());
 
         userProfileCommandService.handle(command);
     }
