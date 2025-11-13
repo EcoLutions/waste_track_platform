892c1da7b7ee9234e81b29bbe2509356955ad0c1
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-13 16:50:14 -0500

    refactor(route): update scheduled date handling to use DateTimeUtils
    Refactor UpdateRouteCommandFromResourceAssembler to convert scheduled date from String to LocalDate using DateTimeUtils for improved date handling.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/routeplanningexecution/interfaces/rest/mappers/fromresourcetocommand/UpdateRouteCommandFromResourceAssembler.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/routeplanningexecution/interfaces/rest/mappers/fromresourcetocommand/UpdateRouteCommandFromResourceAssembler.java
@@ -2,12 +2,13 @@ package com.ecolutions.platform.wastetrackplatform.routeplanningexecution.interf
 
 import com.ecolutions.platform.wastetrackplatform.routeplanningexecution.domain.model.commands.UpdateRouteCommand;
 import com.ecolutions.platform.wastetrackplatform.routeplanningexecution.interfaces.rest.dto.request.UpdateRouteResource;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.utils.DateTimeUtils;
 
 public class UpdateRouteCommandFromResourceAssembler {
     public static UpdateRouteCommand toCommandFromResource(UpdateRouteResource resource) {
         return new UpdateRouteCommand(
             resource.routeId(),
-            resource.scheduledDate()
+            DateTimeUtils.stringToLocalDateOrNull(resource.scheduledDate())
         );
     }
 }
