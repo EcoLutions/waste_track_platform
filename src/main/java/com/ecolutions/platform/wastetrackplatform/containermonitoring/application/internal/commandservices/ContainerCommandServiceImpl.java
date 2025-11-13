5d992735e577177967bb5e2bd62298babc957252
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-13 16:17:46 -0500

    refactor(container): add validation for unique sensor ID and location
    Implement checks to ensure that a container cannot be created or updated with a duplicate sensor ID or location, enhancing data integrity.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/containermonitoring/application/internal/commandservices/ContainerCommandServiceImpl.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/containermonitoring/application/internal/commandservices/ContainerCommandServiceImpl.java
@@ -4,11 +4,14 @@ import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.mod
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.commands.CreateContainerCommand;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.commands.DeleteContainerCommand;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.commands.UpdateContainerCommand;
+import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.valueobjects.SensorId;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.services.command.ContainerCommandService;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.infrastructure.persistence.jpa.repositories.ContainerRepository;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.Location;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
@@ -18,7 +21,10 @@ public class ContainerCommandServiceImpl implements ContainerCommandService {
 
     @Override
     public Optional<Container> handle(CreateContainerCommand command) {
-        // TODO: Add validation of not repeat sensor id, same location
+        if (containerRepository.existsBySensorId(new SensorId(command.sensorId())))
+            throw new IllegalArgumentException("A container with sensor Id" + command.sensorId() + " already exists.");
+        if (containerRepository.existsByLocation(new Location(new BigDecimal(command.latitude()), new BigDecimal(command.longitude()))))
+            throw new IllegalArgumentException("A container with location" + command.latitude() + "," + command.longitude() + " already exists.");
         var container = new Container(command);
         var savedContainer = containerRepository.save(container);
         return Optional.of(savedContainer);
@@ -26,9 +32,16 @@ public class ContainerCommandServiceImpl implements ContainerCommandService {
 
     @Override
     public Optional<Container> handle(UpdateContainerCommand command) {
-        // TODO: Add validation of not repeat sensor id, same location
         var existingContainer = containerRepository.findById(command.containerId())
                 .orElseThrow(() -> new IllegalArgumentException("Container with ID " + command.containerId() + " not found."));
+        if (command.sensorId() != null && !command.sensorId().equals(SensorId.toStringOrNull(existingContainer.getSensorId()))) {
+            if (containerRepository.existsBySensorId(new SensorId(command.sensorId())))
+                throw new IllegalArgumentException("A container with sensor Id" + command.sensorId() + " already exists.");
+        }
+        if (command.latitude() != null && command.longitude() != null) {
+            if (containerRepository.existsByLocation(new Location(new BigDecimal(command.latitude()), new BigDecimal(command.longitude()))))
+                throw new IllegalArgumentException("A container with location" + command.latitude() + "," + command.longitude() + " already exists.");
+        }
         existingContainer.update(command);
         var updatedContainer = containerRepository.save(existingContainer);
         return Optional.of(updatedContainer);
