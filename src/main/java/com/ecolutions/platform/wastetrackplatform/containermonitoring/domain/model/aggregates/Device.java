cdd6d1f5c6a07de53978c87d5fca3168d9e779fd
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-29 15:16:35 -0500

    feat(device): adds Device aggregate
    - Introduces the `Device` aggregate and `DeviceIdentifier` value object.
    - `DeviceIdentifier` enforces validation rules for device IDs.
    - The device aggregate extends AuditableAbstractAggregateRoot.

---
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/containermonitoring/domain/model/aggregates/Device.java
@@ -0,0 +1,20 @@
+package com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.aggregates;
+
+import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.valueobjects.DeviceIdentifier;
+import com.ecolutions.platform.wastetrackplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Device extends AuditableAbstractAggregateRoot<Device> {
+    @Embedded
+    private DeviceIdentifier deviceIdentifier;
+
+    public Device() {
+        super();
+    }
+}
