727f0c254288e585b6032a07aadd341d80552941
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-30 15:17:42 -0500

    refactor(route): enhance route repository with specifications support
    Add JpaSpecificationExecutor to RouteRepository for improved query capabilities. Introduce RouteSpecifications class to encapsulate query logic with various filters for district, driver, vehicle, and status.

--- src/main/java/com/ecolutions/platform/wastetrackplatform/routeplanningexecution/infrastructure/persistence/jpa/repositories/RouteRepository.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/routeplanningexecution/infrastructure/persistence/jpa/repositories/RouteRepository.java
@@ -4,6 +4,7 @@ import com.ecolutions.platform.wastetrackplatform.routeplanningexecution.domain.
 import com.ecolutions.platform.wastetrackplatform.routeplanningexecution.domain.model.valueobjects.RouteStatus;
 import com.ecolutions.platform.wastetrackplatform.shared.domain.model.valueobjects.DistrictId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface RouteRepository extends JpaRepository<Route, String> {
+public interface RouteRepository extends JpaRepository<Route, String>, JpaSpecificationExecutor<Route> {
     @Query("SELECT r FROM Route r WHERE r.districtId = :districtId AND r.status IN :activeStatuses")
     List<Route> findActiveRoutesByDistrictId(@Param("districtId") DistrictId districtId, @Param("activeStatuses") List<RouteStatus> activeStatuses);
     List<Route> findByStatus(RouteStatus routeStatus);
