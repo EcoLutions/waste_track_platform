6f5874b71aaf24da1851e80444ff9e6793967616
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2025-12-03 10:32:19 -0500

    feat: implement EmptyContainerCommand and handling in ContainerCommandService

--- src/main/java/com/ecolutions/platform/wastetrackplatform/containermonitoring/domain/services/command/ContainerCommandService.java
+++ src/main/java/com/ecolutions/platform/wastetrackplatform/containermonitoring/domain/services/command/ContainerCommandService.java
@@ -2,6 +2,7 @@ package com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.se
 
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.aggregates.Container;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.commands.CreateContainerCommand;
+import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.commands.EmptyContainerCommand;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.commands.UpdateContainerCommand;
 import com.ecolutions.platform.wastetrackplatform.containermonitoring.domain.model.commands.DeleteContainerCommand;
 
@@ -11,4 +12,5 @@ public interface ContainerCommandService {
     Optional<Container> handle(CreateContainerCommand command);
     Optional<Container> handle(UpdateContainerCommand command);
     Boolean handle(DeleteContainerCommand command);
+    Optional<Container> handle(EmptyContainerCommand command);
 }
