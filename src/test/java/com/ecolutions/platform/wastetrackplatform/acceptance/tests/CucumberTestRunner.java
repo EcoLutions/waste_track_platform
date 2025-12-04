ec6e742e1f1ab1a2148c6feda3b757e4d8285ce1
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-12-04 11:24:16 -0500

    test(acceptance): update Cucumber tests to use command objects for driver and sensor readings
    Refactor tests to utilize CreateDriverCommand and CreateSensorReadingCommand for improved clarity and maintainability. Adjusted feature file messages for consistency.

--- src/test/java/com/ecolutions/platform/wastetrackplatform/acceptance/tests/CucumberTestRunner.java
+++ src/test/java/com/ecolutions/platform/wastetrackplatform/acceptance/tests/CucumberTestRunner.java
@@ -4,8 +4,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-//@Suite
-//@IncludeEngines("cucumber")  // Indica que use el engine de Cucumber
-//@SelectClasspathResource("features")  // Ruta a tus archivos .feature
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
 public class CucumberTestRunner {
 }
